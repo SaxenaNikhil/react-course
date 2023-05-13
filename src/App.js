60a3b532c98713b7537fa561767fba86915f630e
Nikhil Saxena <nikhilsaxena003@gmail.com>
2023-05-14 01:24:20 +0530

    Working with props and accessing the data from parent to child component and updating the card heading dynamically

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
     //Note - the code written here is not HTML
     <div>
       <h1>My Todos</h1>
-      <Todo />
+      
+      {/* here we are passing the custom text heading for each todo */}
+      <Todo text='Learn React' />
+      <Todo text='Master React' />
+      <Todo text='Explore the full React course' />
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,13 @@
 //a component in react is just a function
 //Note - the name of the function should be in capital letters
-function Todo() {
+function Todo(props) {
+    //now to pickup the 'text' value from the app.js Todo component, so we will use props which will accept the parameters in our function component.
+
+    //here props is a js object, where all the attributes added on the element are available as key value pair in this props object, where the attribute name 'text' will be the keys and 'Learn React', 'Master React' etc.. will be the values
+
     return (
         <div className="card">
-            <h2>TITLE</h2>
+            <h2>{props.text}</h2>
             <div className="actions">
                 <button className="btn">Delete</button>
             </div>
